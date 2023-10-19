c69ac205158e156b9fb5ba205b8297c4e91881fe
jabber <2898117012@qq.com>
2023-10-19 23:03:08 +0800

    init

--- shopeo-plugin-scaffold.php
+++ shopeo-plugin-scaffold.php
@@ -13,11 +13,64 @@
  * Requires PHP: 5.6
  */
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once 'vendor/autoload.php';
 }
 
+if ( ! defined( 'SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE' ) ) {
+	define( 'SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! function_exists( 'shopeo_plugin_scaffold_activation' ) ) {
+	function shopeo_plugin_scaffold_activation() {
+
+	}
+}
+
+register_activation_hook( SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE, 'shopeo_plugin_scaffold_activation' );
+
+if ( ! function_exists( 'shopeo_plugin_scaffold_deactivation' ) ) {
+	function shopeo_plugin_scaffold_deactivation() {
+
+	}
+}
+
+register_deactivation_hook( SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE, 'shopeo_plugin_scaffold_deactivation' );
+
+if ( ! function_exists( 'shopeo_plugin_scaffold_init' ) ) {
+	function shopeo_plugin_scaffold_init() {
+
+		//load text domain
+		load_plugin_textdomain( 'shopeo-plugin-scaffold', false, dirname( plugin_basename( SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE ) ) . '/languages' );
+	}
+}
+
+add_action( 'init', 'shopeo_plugin_scaffold_init' );
+
+add_action( 'admin_enqueue_scripts', function () {
+	$plugin_version = get_plugin_data( SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE )['Version'];
+	//style
+
+	//script
+	wp_enqueue_script( 'shopeo-plugin-scaffold-admin-script', plugins_url( '/assets/js/admin.js', SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE ), array( 'jquery' ), $plugin_version );
+	wp_localize_script( 'shopeo-plugin-scaffold-admin-script', 'shopeo_plugin_scaffold', array(
+		'ajax_url' => admin_url( 'admin-ajax.php' )
+	) );
+} );
+
+add_action( 'wp_enqueue_scripts', function () {
+	$plugin_version = get_plugin_data( SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE )['Version'];
+	//style
+	wp_enqueue_style( 'shopeo-plugin-scaffold-style', plugins_url( '/assets/css/style.css', SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE ), array(), $plugin_version );
+	wp_style_add_data( 'shopeo-plugin-scaffold-style', 'rtl', 'replace' );
+
+	//script
+	wp_enqueue_script( 'shopeo-plugin-scaffold-script', plugins_url( '/assets/js/app.js', SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE ), array( 'jquery' ), $plugin_version );
+	wp_localize_script( 'shopeo-plugin-scaffold-script', 'shopeo_plugin_scaffold', array(
+		'ajax_url' => admin_url( 'admin-ajax.php' )
+	) );
+} );
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
 
-mix.js('src/assets/js/app.js', 'assets/js').sass('src/assets/scss/style.scss', 'assets/css', [], [require('postcss-import'), require('autoprefixer'),]).sass('src/assets/scss/style-rtl.scss', 'assets/css', [], [require('postcss-import'), require('rtlcss'), require('autoprefixer'),]);
+mix.js('src/assets/js/admin.js', 'assets/js').js('src/assets/js/app.js', 'assets/js').sass('src/assets/scss/style.scss', 'assets/css', [], [require('postcss-import'), require('autoprefixer'),]).sass('src/assets/scss/style-rtl.scss', 'assets/css', [], [require('postcss-import'), require('rtlcss'), require('autoprefixer'),]);
