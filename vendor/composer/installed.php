a831d2f0c665fa565439036334e3a1df58d8e1d3
jabber <2898117012@qq.com>
2023-10-19 19:51:45 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shopeo/shopeo-plugin-scaffold',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '88712e53eb7ff333732569982e82959ce3f4ae0c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shopeo/shopeo-plugin-scaffold' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '88712e53eb7ff333732569982e82959ce3f4ae0c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
