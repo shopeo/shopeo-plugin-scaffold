88712e53eb7ff333732569982e82959ce3f4ae0c
jabber <2898117012@qq.com>
2023-10-19 19:47:33 +0800

    init

---
+++ shopeo-plugin-scaffold.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: SHOPEO WP Core
+ * Plugin URI: https://wordpress.org/plugins/shopeo-wp-core
+ * Description: The easy way to customize your WordPress <code>wp-login.php</code> screen!
+ * Author: Shopeo
+ * Version: 0.0.2
+ * Author URI: https://shopeo.cn
+ * License: GPL2+
+ * Text Domain: shopeo-wp-core
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+	require_once 'vendor/autoload.php';
+}
+
